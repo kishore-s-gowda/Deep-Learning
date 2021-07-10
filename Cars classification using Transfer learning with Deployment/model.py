1a2ce53983c4f13c4dad8522cfec3f23698b16fe
kishore-s-gowda <63787862+kishore-s-gowda@users.noreply.github.com>
2021-07-10 09:42:15 +0530

    Add files via upload

--- Cars classification using Transfer learning with Deployment/model.py
+++ Cars classification using Transfer learning with Deployment/model.py
@@ -1,1 +1,30 @@
+import gradio as gr
+import tensorflow as tf
+import numpy as np
+import requests
+from tensorflow.keras.models import load_model
+class_names=['Creta', 'Ford EcoSport', 'Hyundai Santro', 'Innova', 'MG Hector', 'Mahindra Bolero',
+ 'Maruti Baleno', 'Maruti Eeco', 'Maruti Vitara Brezza', 'Nano', 'Range Rover', 'Renault Duster',
+ 'Scorpio', 'hyundai i20', 'hyundai venue', 'mahindra thar', 'maruti swift', 'mini cooper']
+model_load=load_model('best_model.h5')
+def prediction(image):
+    image = image.reshape((1, 224, 224, 3))
+    image=tf.keras.applications.mobilenet.preprocess_input(image)
+    prediction = model_load.predict(image).flatten()
+    return {class_names[i]: float(prediction[i]) for i in range(18)}
+# Use predefined input and output objects from gradio
+image1 = gr.inputs.Image(shape=(224,224))
+label1 = gr.outputs.Label(num_top_classes=18)
+
+# Gradio interface to input an image and see its prediction with percentage confidence
+gr.Interface(fn=prediction, inputs=image1, outputs=label1,
+             #theme="huggingface",
+             title="CLASSIFICATION OF CAR MODELS USING CONVOLUTIONAL NEURAL NETWORK",
+             description =" Select an image and hit submit to see its classification",
+             allow_flagging=False,
+             layout="vertical",
+             live=True,
+             capture_session=True,
+             interpretation='default').launch(debug='True',share=True)
+
 
